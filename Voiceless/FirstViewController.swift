70428b4bacf5ea393f92725ffd849fc75a35e336
Sylvain Gamel <sylvain@SGinTech.fr>
2017-04-30 01:19:28 +0200

    Initial Commit

---
+++ Voiceless/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Voiceless
+//
+//  Created by Sylvain on 30/04/2017.
+//  Copyright © 2017 S.G. inTech. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
